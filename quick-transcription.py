dfbb60eb907e18ef68e87f21f303085681e4e3c2
Robin Sharma <robinrythm123@gmail.com>
2023-10-19 03:53:46 +0530

    add docstring

--- quick-transcription.py
+++ quick-transcription.py
@@ -6,8 +6,12 @@ from faster_whisper import WhisperModel
 
 
 class QuickTranscription:
-
+    '''
+    Provides a public method 'listen' to start the audio stream along 
+    with the live transcription.
+    '''
     def __init__(self, model_size: str = "base"):
+        print("INITIALIZING...")
         self._model = WhisperModel(
             model_size, 
             device="cpu", 
@@ -20,6 +24,7 @@ class QuickTranscription:
         self._rate = 16000
         self._vad_filter = True
         self._input_device_index = 1
+        print("READY")
     
     def listen(self) -> None:
         pa = pyaudio.PyAudio()
@@ -30,7 +35,7 @@ class QuickTranscription:
             input=True,
             frames_per_buffer=self._chunk,
             input_device_index=self._input_device_index,
-            stream_callback=self.transcribe,
+            stream_callback=self._transcribe,
         )
         stream.start_stream()
         while stream.is_active():
@@ -40,7 +45,7 @@ class QuickTranscription:
         stream.close()
         pa.terminate()
     
-    def transcribe(self, in_data, frame_count, time_info, status) -> tuple:
+    def _transcribe(self, in_data, frame_count, time_info, status) -> tuple:
         audio_data = np.frombuffer(in_data, np.int16).flatten().astype(np.float32) / 32768.0
         segments, _ = self._model.transcribe(
             audio_data, 
